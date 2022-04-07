c6ee1ca199a67d3bfcd0decdc356890615df24bf
Anant <anantanant2015@gmail.com>
2022-04-08 03:06:30 +0530

    Adds feature with tests

--- test/bob_test.exs
+++ test/bob_test.exs
@@ -2,7 +2,17 @@ defmodule BobTest do
   use ExUnit.Case
   doctest Bob
 
-  test "greets the world" do
-    assert Bob.hello() == :world
+  test "sending message to bob" do
+    assert Bob.message("") == "Fine. Be that way!"
+
+    assert Bob.message("HOW ARE YOU?") == "Calm down, I know what I’m doing!"
+
+    assert Bob.message("How are you?") == "Sure."
+
+    assert Bob.message("TELL ME YOUR NAME") == "Whoa, chill out!"
+
+    assert Bob.message(0) == "Whatever."
+
+    assert Bob.message() == "Whatever."
   end
 end
